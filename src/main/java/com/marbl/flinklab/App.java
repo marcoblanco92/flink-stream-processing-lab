afc60381d1c3cea5b475a9efc2d53e0373222ebd
MARCO BLANCO <marco.blanco@intesasanpaolo.com>
2026-03-22 18:44:10 +0100

    First commit - pom setting.

---
+++ src/main/java/com/marbl/flinklab/App.java
@@ -0,0 +1,13 @@
+package com.marbl.flinklab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
